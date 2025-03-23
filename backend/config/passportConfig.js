331bcc99c52313ba2f73a4da7b7f26ea9808cbcb
adya tiwari <adya.tiwari20@st.niituniversity.in>
2025-03-23 12:01:17 +0530

    deployed

--- backend/config/passportConfig.js
+++ backend/config/passportConfig.js
@@ -23,8 +23,9 @@ passport.deserializeUser(async (id, done) => {
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: 'https://moon-ai-one.vercel.app/auth/google/callback',
-  passReqToCallback: true, // Add this to pass the request object to the callback
+  callbackURL: 'https://moonai-production.up.railway.app/auth/google/callback',
+  // callbackURL: 'https://moon-ai-one.vercel.app/auth/google/callback',
+  passReqToCallback: true, 
 }, async (req, accessToken, refreshToken, profile, done) => {
   try {
     let user = await User.findOne({ googleId: profile.id });
